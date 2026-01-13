eedeb6c83edde3753f055df025c251258e03a020
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-01-13 01:31:06 +0100

    Creating the signals.py and updating the Apps.py and the init files

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Importing signals ensures they are registered when the app is ready
+        import properties.signals
