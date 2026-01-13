10d133e17e1ca0aadf88b0cf3d5e7d693960168c
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-01-13 01:21:03 +0100

    Creating the Views and mapping the url

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)  # Cache the result for 15 minutes
+def property_list(request):
+    """
+    Fetches all properties and returns them as a JSON response.
+    Subsequent requests within 15 mins will be served from Redis.
+    """
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    data = list(properties)
+    return JsonResponse(data, safe=False)
